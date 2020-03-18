ac678872193c87b1805da90226977fae716358ff
jvsena42 <jvsena16@gmail.com>
2020-03-18 17:02:56 -0300

    mascara de moeda

--- app/src/main/java/com/jvsena/olxclone/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/jvsena/olxclone/activity/CadastrarAnuncioActivity.java
@@ -3,14 +3,41 @@ package com.jvsena.olxclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.jvsena.olxclone.R;
 
+import java.util.Locale;
+
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
     }
+
+    private void inicializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+
+
+        //Configurar localidade para pt-Br
+        Locale locale = new Locale("pt","BR");
+        campoValor.setLocale(locale);
+    }
+
+    public void salvarAnuncio(View view){
+        String valor = campoValor.getText().toString();
+    }
+
+
 }
