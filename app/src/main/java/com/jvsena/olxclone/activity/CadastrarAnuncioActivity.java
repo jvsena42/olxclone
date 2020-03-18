f9186444f0ec1cf329f3ba1679ebdb12f860fd28
jvsena42 <jvsena16@gmail.com>
2020-03-18 19:36:24 -0300

    configurando permissões

--- app/src/main/java/com/jvsena/olxclone/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/jvsena/olxclone/activity/CadastrarAnuncioActivity.java
@@ -1,13 +1,19 @@
 package com.jvsena.olxclone.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.jvsena.olxclone.R;
+import com.jvsena.olxclone.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
 import java.util.Locale;
@@ -18,10 +24,18 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
 
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes,this,1);
 
         inicializarComponentes();
     }
@@ -41,5 +55,31 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         String valor = campoValor.getText().toString();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        for (int permissaoResultado : grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar o app é necessário acessar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
